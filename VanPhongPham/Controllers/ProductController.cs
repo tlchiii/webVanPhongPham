8395e7042d1e03cdd469a93800a988137057121d
Linh Chi <chi@gmail>
2024-10-30 12:51:19 +0700

    first commit

---
+++ VanPhongPham/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VanPhongPham.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
