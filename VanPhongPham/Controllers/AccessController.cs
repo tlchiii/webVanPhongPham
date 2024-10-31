4ba7e1150488a639ef0e050253f3f8a8d0e050f4
thuong2211 <thuongphuongthang@gmail.com>
2024-10-31 13:19:01 +0700

    fix conflict

---
+++ VanPhongPham/Controllers/AccessController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VanPhongPham.Controllers
+{
+    public class AccessController : Controller
+    {
+         
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
